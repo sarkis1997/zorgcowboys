d932f10c9ce92fb45691b546cc58b4d49cea39a7
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-12-13 17:48:48 +0100

    added regex and csv parser to array

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+import { readFile } from "./utils/uploadFile.js";
+
 function startApp() {
+	readFile();
 	console.log('app started')
 }
 
--- js/utils/uploadFile.js
+++ js/utils/uploadFile.js
@@ -1,4 +1,36 @@
-export function readFile() {
-	console.log('clicked');
-};
+import '../modules/jquery-csv.js'
 
+export function readFile() {
+
+	var result = [];
+
+	const input = document.querySelector('input[type="file"]');
+	input.addEventListener('change', function () {
+
+		const reader = new FileReader();
+		reader.onload = function () {
+
+			const lines = reader.result.split("\n");
+
+			var headers = lines[0].replace(/"/g,'').split(",");
+
+			for(var i=1;i<lines.length;i++){
+				var obj = {};
+				var currentline=lines[i].replace(/"/g,'').split(",");
+
+				for(var j=0;j<headers.length;j++){
+					obj[headers[j]] = currentline[j];
+				}
+				result.push(obj);
+
+			}
+		};
+		reader.readAsText(input.files[0])
+
+
+	}, false);
+
+	console.log(result)
+
+
+}
