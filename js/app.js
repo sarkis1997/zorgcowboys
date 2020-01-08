bc58d93e2962e0495def8c8d4762b49d9a4ec4fa
Sarkis Moeradjan <smoeradjan@gmail.com>
2020-01-08 19:31:59 +0100

    categorized data on year

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 import { scatterPlot } from "./utils/createScatterPlot.js";
 
-
 d3.json("datasets/dataset.json")
+	.then(function (data) {
+		let j2011 = [], j2012 = [], j2013 = [], j2014 = [], j2015 = [], j2016 = [], j2017 = [], j2018 = [];
+		data.map(item => {
+			switch (item.jaar) {
+				case 2011:
+					j2011.push(item);
+					break;
+				case 2012:
+					j2012.push(item);
+					break;
+				case 2013:
+					j2013.push(item);
+					break;
+				case 2014:
+					j2014.push(item);
+					break;
+				case 2015:
+					j2015.push(item);
+					break;
+				case 2016:
+					j2016.push(item);
+					break;
+				case 2017:
+					j2017.push(item);
+					break;
+				case 2018:
+					j2018.push(item);
+					break;
+			}
+		});
+		return { j2011, j2012, j2013, j2014, j2015, j2016, j2017, j2018}
+	})
 	.then(function(data) {
 		startApp(data)
 	});
@@ -10,4 +41,3 @@ d3.json("datasets/dataset.json")
 export function startApp(data) {
 	scatterPlot(data);
 }
-
--- js/utils/createScatterPlot.js
+++ js/utils/createScatterPlot.js
@@ -1,41 +1,69 @@
 export function scatterPlot(data) {
-	console.log(data);
 
-	const body = d3.select('#map-holder');
-	let margin = {top: 50, right: 50, bottom: 50, left: 50};
-	let width = innerWidth - margin.left - margin.right;
-	let height = innerHeight - margin.top - margin.bottom;
+	let j11 = data.j2011;
+	let j12 = data.j2012;
+	let j13 = data.j2013;
+	let j14 = data.j2014;
+	let j15 = data.j2015;
+	let j16 = data.j2016;
+	let j17 = data.j2017;
+	let j18 = data.j2018;
+
+	const canvas = d3.select('#map-holder');
+
+	const xScale = d3.scaleLinear();
+	const xAxis = d3.axisBottom(xScale);
+
+
 
 	function myResponsiveComponent(container, props) {
+
 		let svg = container.selectAll('svg').data([null]);
 		svg = svg.enter().append('svg')
+			.attr('class', 'mainSVG')
 			.merge(svg)
 			.attr('width', props.width)
 			.attr('height', props.height);
 
-		let group = svg.selectAll('g').data([null]);
-		group
-			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-			.enter().append("g")
-			.merge(group);
+		xScale
+			.domain(d3.extent(data, d => { return d.omzet }))
+			.range([50, props.width - 80])
+			.nice();
 
-		const xScale = d3.scaleLinear()
-			.domain(d3.extent(data.map(item => {
-				// console.log(item)
-				return item.omzet;
-			})))
-			.range([0, width]);
-		const xAxis = d3.axisBottom(xScale)
-			.ticks(width / 70);
+		xAxis
+			.ticks(props.width / 100);
 
-		let xAxisG = svg.selectAll('.x-axis').data([null]);
-		xAxisG = xAxisG
+		let xAxisGroup = d3.select('.mainSVG').selectAll('.x-axis').data([null]);
+		xAxisGroup = xAxisGroup
 			.enter().append('g')
 			.attr('class', 'x-axis')
-			.merge(xAxisG);
-		xAxisG.call(xAxis);
+			.merge(xAxisGroup)
+			.attr('width', props.width)
+			.attr('height', props.height);
+		xAxisGroup.call(xAxis);
+		//
+		// let valueline = d3.line()
+		// 	.x(function (d) {
+		// 		return x(d.omzet);
+		// 	});
+		//
+		// let path = svg.selectAll("circle")
+		// 	.data(data)
+		// 	.enter().append("circle")
+		// 	.attr("r", 5)
+		// 	.attr("cx", function (d) {
+		// 		if (isNaN(d.omzet)) {
+		// 			d.omzet = 0;
+		// 		}
+		// 		return xScale(d.omzet)
+		// 	})
+		// 	.attr("cy", 30)
+		// 	.attr("stroke", "#32CD32")
+		// 	.attr("stroke-width", 1.5)
+		// 	.attr("fill", "#FFFFFF");
 
-		const xAxisLabel = xAxisG.selectAll('.x-as-label').data([null]);
+
+		const xAxisLabel = xAxisGroup.selectAll('.x-as-label').data([null]);
 		xAxisLabel
 			.enter().append('text')
 			.attr('class', 'x-as-label')
@@ -47,18 +75,14 @@ export function scatterPlot(data) {
 	}
 
 	function render() {
-		myResponsiveComponent(body, {
+		myResponsiveComponent(canvas, {
 			width: document.body.clientWidth,
 			height: document.body.clientHeight,
 		});
 	}
 
 	render();
-
 	window.addEventListener('resize', render);
-
-
-
 }
 
 
