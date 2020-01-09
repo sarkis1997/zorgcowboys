0ee983d6a0d68fecc61ac9f4850667a2b963251a
Sarkis Moeradjan <smoeradjan@gmail.com>
2020-01-09 01:44:47 +0100

    structured better

--- js/utils/createScatterPlot.js
+++ js/utils/createScatterPlot.js
@@ -2,34 +2,32 @@ import { checkYearInput } from "./checkSelectedYear.js";
 
 export function scatterPlot(data) {
 	createScatterPlot(data.j2018);
-
 	d3.select('#selectYear').on("change", function () {
 		let inputData = checkYearInput(data);
 		createScatterPlot(inputData);
 	});
+}
 
-	function createScatterPlot(dataset) {
-		let width = 800,
-			height = 100;
+function createScatterPlot(dataset) {
+	let width = innerWidth,
+		height = 100;
 
-		// Append SVG
-		let svg = d3.select('#viz-holder')
-			.append('svg')
-			.attr('width', width)
-			.attr('height', height);
+	// Append SVG
+	let svg = d3.select('#viz-holder')
+		.append('svg')
+		.attr('width', width)
+		.attr('height', height);
 
-		// Create scale
-		let scale = d3.scaleLinear()
-			.domain(d3.extent(dataset, d => { if(isNaN(d.omzet)){d.omzet=0} return d.omzet }))
-			.range([0, width - 100]);
+	// Create scale
+	let scale = d3.scaleLinear()
+		.domain(d3.extent(dataset, d => { if(isNaN(d.omzet)){d.omzet=0} return d.omzet }))
+		.range([0, width - 100]);
 
-		let xAxis = d3.axisBottom()
-			.scale(scale);
+	let xAxis = d3.axisBottom()
+		.scale(scale);
 
-		//Append group and insert axis
-		svg.append("g")
-			.call(xAxis);
-
-	}
+	//Append group and insert axis
+	svg.append("g")
+		.call(xAxis);
 
 }
