020dec55f5b8a7f3d1f1c58d3d71c3f785632834
Sarkis Moeradjan <smoeradjan@gmail.com>
2020-01-18 16:09:18 +0100

    minified scripts and css

--- js/utils/createScatterPlot.js
+++ js/utils/createScatterPlot.js
@@ -6,54 +6,15 @@ export function scatterPlot(data) {
 	d3.select('#selectYear').on("change", function () {
 		let svg = d3.select('#viz-holder');
 		let circles = svg.selectAll(".circle");
-		let over10 = document.querySelector('.filterOver10');
-		let negative = document.querySelector('.filterNegative');
-		let less10 = document.querySelector('.filterUnder10');
-		document.querySelector('.filterUnder10').addEventListener('click', filterLess10);
-		document.querySelector('.filterOver10').addEventListener('click', filterOver10);
-		document.querySelector('.filterNegative').addEventListener('click', filterNegative);
-		if (document.querySelector('.chosen')) {
-			document.querySelector('.chosen').classList.remove('chosen')
-		}
-		function filterLess10() {
-			negative.classList.remove('chosen'); over10.classList.remove('chosen');
-			if (less10.classList.contains('chosen')) {
-				circles.attr("display", 'block');
-				less10.classList.remove('chosen')
-			}
-			else {
-				circles.attr("display", function (d) {
-					if (!(d.perc_winst < 10)) { return 'none'};
-					less10.classList.add('chosen');
-				});
-			}
-		}
-		function filterOver10() {
-			negative.classList.remove('chosen'); less10.classList.remove('chosen');
-			if (over10.classList.contains('chosen')) {
-				circles.attr("display", 'block');
-				over10.classList.remove('chosen')
-			}
-			else {
-				circles.attr("display", function (d) {
-					if (!(d.perc_winst >= 10)) { return 'none'};
-					over10.classList.add('chosen');
-				});
-			}
-		}
-		function filterNegative() {
-			over10.classList.remove('chosen'); less10.classList.remove('chosen');
-			if (negative.classList.contains('chosen')) {
-				circles.attr("display", 'block');
-				negative.classList.remove('chosen')
-			}
-			else {
-				circles.attr("display", function (d) {
-					if (!(d.perc_winst < 0)) { return 'none'};
-					negative.classList.add('chosen');
-				});
-			}
-		}
+
+		let over10=document.querySelector('.filterOver10');let negative=document.querySelector('.filterNegative');let less10=document.querySelector('.filterUnder10');document.querySelector('.filterUnder10').addEventListener('click',filterLess10);document.querySelector('.filterOver10').addEventListener('click',filterOver10);document.querySelector('.filterNegative').addEventListener('click',filterNegative);if(document.querySelector('.chosen')){document.querySelector('.chosen').classList.remove('chosen')}
+		function filterLess10(){negative.classList.remove('chosen');over10.classList.remove('chosen');if(less10.classList.contains('chosen')){circles.attr("display",'block');less10.classList.remove('chosen')}
+		else{circles.attr("display",function(d){if(!(d.perc_winst<10)){return'none'};less10.classList.add('chosen')})}}
+		function filterOver10(){negative.classList.remove('chosen');less10.classList.remove('chosen');if(over10.classList.contains('chosen')){circles.attr("display",'block');over10.classList.remove('chosen')}
+		else{circles.attr("display",function(d){if(!(d.perc_winst>=10)){return'none'};over10.classList.add('chosen')})}}
+		function filterNegative(){over10.classList.remove('chosen');less10.classList.remove('chosen');if(negative.classList.contains('chosen')){circles.attr("display",'block');negative.classList.remove('chosen')}
+		else{circles.attr("display",function(d){if(!(d.perc_winst<0)){return'none'};negative.classList.add('chosen')})}}
+
 		let pNode = document.getElementById('viz-holder');
 		while (pNode.firstChild) { pNode.removeChild(pNode.firstChild);}
 		let rangeField = document.getElementById('slider-container');
@@ -168,82 +129,16 @@ function createScatterPlot(dataset) {
 	let counter = d3.select('.amountContainer')
 		.text(function () { return dataset.length });
 
-	let over10 = document.querySelector('.filterOver10');
-	let negative = document.querySelector('.filterNegative');
-	let less10 = document.querySelector('.filterUnder10');
-
-	function searchFunction(e) {
-		let filterNodes = dataset.filter(zorgbedrijf => zorgbedrijf.bedrijfsnaam.toLowerCase().includes(e.target.value.toLowerCase()));
-		circles.attr("display", function (d) { if (!filterNodes.includes(d)) {return 'none'} })
-	}
-
-	function filterLess10() {
-		negative.classList.remove('chosen'); over10.classList.remove('chosen');
-		if (less10.classList.contains('chosen')) {
-			circles.attr("display", 'block');
-			less10.classList.remove('chosen')
-		}
-		else {
-			circles.attr("display", function (d) {
-				if (!(d.perc_winst < 10)) { return 'none'};
-				less10.classList.add('chosen');
-			});
-		}
-	}
-	function filterOver10() {
-		negative.classList.remove('chosen'); less10.classList.remove('chosen');
-		if (over10.classList.contains('chosen')) {
-			circles.attr("display", 'block');
-			over10.classList.remove('chosen')
-		}
-		else {
-			circles.attr("display", function (d) {
-				if (!(d.perc_winst >= 10)) { return 'none'};
-				over10.classList.add('chosen');
-			});
-		}
-	}
-	function filterNegative() {
-		over10.classList.remove('chosen'); less10.classList.remove('chosen');
-		if (negative.classList.contains('chosen')) {
-			circles.attr("display", 'block');
-			negative.classList.remove('chosen')
-		}
-		else {
-			circles.attr("display", function (d) {
-				if (!(d.perc_winst < 0)) { return 'none'};
-				negative.classList.add('chosen');
-			});
-		}
-	}
-
-
-	function handleClick(d) {
-		let sb = document.getElementById("sidebar");
-		if (sb.style.width == 0 || sb.style.width == "0px" ) {
-			sb.style.width = "450px";
-		} else {
-			sb.style.width = "0";
-		}
-	}
-
-	function handleMouseOver(d) {
-		tooltip
-			// .transition()
-			// .duration(200)
-			.style("opacity", 1)
-			.style("left", (d3.event.pageX - 200) + "px")
-			.style("top", (d3.event.pageY -265) + "px");
-		d3.select('.ttNaam').html(d.bedrijfsnaam)
-		d3.select('.ttWinst').html('<span>winst:</span>' + '<span> € ' + Number(d.winst).toLocaleString() + '</span>');
-		d3.select('.ttOmzet').html('<span>omzet:</span>' + '<span> €' + Number(d.omzet).toLocaleString() + '</span>');
-		d3.select('.ttPercentage').html('<span>winstpercentage:</span>' + '<span>' + d.perc_winst + ' %</span>');
-	}
-
-	function handleMouseOut() {
-		tooltip
-			// .transition()
-			// .duration(200)
-			.style("opacity", 0);
-	}
+	let over10=document.querySelector('.filterOver10');let negative=document.querySelector('.filterNegative');let less10=document.querySelector('.filterUnder10');function searchFunction(e){let filterNodes=dataset.filter(zorgbedrijf=>zorgbedrijf.bedrijfsnaam.toLowerCase().includes(e.target.value.toLowerCase()));circles.attr("display",function(d){if(!filterNodes.includes(d)){return'none'}})}
+	function filterLess10(){negative.classList.remove('chosen');over10.classList.remove('chosen');if(less10.classList.contains('chosen')){circles.attr("display",'block');less10.classList.remove('chosen')}
+	else{circles.attr("display",function(d){if(!(d.perc_winst<10)){return'none'};less10.classList.add('chosen')})}}
+	function filterOver10(){negative.classList.remove('chosen');less10.classList.remove('chosen');if(over10.classList.contains('chosen')){circles.attr("display",'block');over10.classList.remove('chosen')}
+	else{circles.attr("display",function(d){if(!(d.perc_winst>=10)){return'none'};over10.classList.add('chosen')})}}
+	function filterNegative(){over10.classList.remove('chosen');less10.classList.remove('chosen');if(negative.classList.contains('chosen')){circles.attr("display",'block');negative.classList.remove('chosen')}
+	else{circles.attr("display",function(d){if(!(d.perc_winst<0)){return'none'};negative.classList.add('chosen')})}}
+	function handleClick(d){console.log(d)
+		let sb=document.getElementById("sidebar");if(sb.style.width==0||sb.style.width=="0px"){sb.style.width="450px"}else{sb.style.width="0"}}
+	function handleMouseOver(d){tooltip.style("opacity",1).style("left",(d3.event.pageX-200)+"px").style("top",(d3.event.pageY-265)+"px");d3.select('.ttNaam').html(d.bedrijfsnaam)
+		d3.select('.ttWinst').html('<span>winst:</span>'+'<span> € '+Number(d.winst).toLocaleString()+'</span>');d3.select('.ttOmzet').html('<span>omzet:</span>'+'<span> €'+Number(d.omzet).toLocaleString()+'</span>');d3.select('.ttPercentage').html('<span>winstpercentage:</span>'+'<span>'+d.perc_winst+' %</span>')}
+	function handleMouseOut(){tooltip.style("opacity",0)}
 }
